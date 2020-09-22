a0f42db7f8b4acd982da4d568655f9e87431dcf9
michael00987 <hyeongkwangseok@gmail.com>
2020-09-22 20:04:01 +0900

    rest API 설명

---
+++ server/server6.js
@@ -0,0 +1,17 @@
+/*
+
+GET wwww.micahel.com/users
+해당 주소에서 유저들의 정보를 가져와라
+
+POST wwww.micahel.com/users
+해당 주소에 유저의 정보를 등록하라
+
+PUT wwww.micahel.com/users/1
+해당 주소에서 특정 유저 정보를 통체로 교체해라
+
+PATCH wwww.micahel.com/users/2
+해당 주소에서 특정 유저 정보의 일부를 수정해라
+
+DELETE wwww.micahel.com/users/5
+해당 주소에서 특정 유저 정보를 제거하라
+*/
