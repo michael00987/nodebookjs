c7092d46077b99f6c06929dddd04ff21218f5fd5
michael00987 <hyeongkwangseok@gmail.com>
2020-09-22 21:18:25 +0900

    https 설명

---
+++ https/https.js
@@ -0,0 +1,46 @@
+const { fstat } = require('fs');
+const http = require('http'); // 1.1
+const https = require('https');
+const http2 = require('http2'); // https 와 비슷
+
+http
+  .createServer((req, res) => {
+    res.end('http server');
+  })
+  .listen(80);
+https
+  .createServer(
+    {
+      cert: fstat.readFileSync('도메인 인증서 경로'),
+      key: fstat.readFileSync('도메인 인증서 경로'),
+      ca: [
+        fs.readFileSync('상위 인증서 경로'),
+        fs.readFileSync('상위 인증서 경로'),
+        fs.readFileSync('상위 인증서 경로'),
+        fs.readFileSync('상위 인증서 경로'),
+      ],
+    },
+    (req, res) => {
+      res.end('https server');
+    }
+  )
+  .listen(443);
+
+// http2 는 https 기반이기 때문에 인증서가 필요하다
+http2
+  .createSecureServer(
+    {
+      cert: fstat.readFileSync('도메인 인증서 경로'),
+      key: fstat.readFileSync('도메인 인증서 경로'),
+      ca: [
+        fs.readFileSync('상위 인증서 경로'),
+        fs.readFileSync('상위 인증서 경로'),
+        fs.readFileSync('상위 인증서 경로'),
+        fs.readFileSync('상위 인증서 경로'),
+      ],
+    },
+    (req, res) => {
+      res.end('https server');
+    }
+  )
+  .listen(443);
