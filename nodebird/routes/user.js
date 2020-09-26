010bc1cd87c78767e150e6c856ef972dfb5ff799
michael00987 <hyeongkwangseok@gmail.com>
2020-09-27 08:49:00 +0900

    follow 끊기

--- nodebird/routes/user.js
+++ nodebird/routes/user.js
@@ -14,4 +14,14 @@ router.post('/:id/follow', isLoggedIn, async (req, res, next) => {
   }
 });
 
+router.post('/:id/unfollow', isLoggedIn, async (req, res, next) => {
+  try {
+    const user = await User.findOne({where: {id: req.user.id}}); // 현재 로그인한 나 자신을 찾아서
+    await user.removeFollowing(parseInt(req.params.id, 10)); // 나에게 나를 팔로윙한사람츨 찾아서 넣는다.
+    res.send('success');
+  } catch (e) {
+    console.error(e);
+    next(e);
+  }
+});
 module.exports = router;
