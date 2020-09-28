0d1e199485bcfca4e19794f1c3c40be19b1bf987
michael00987 <hyeongkwangseok@gmail.com>
2020-09-28 09:07:12 +0900

    render 버그 수정, clientSecret 생성 api

--- nodebird-api/routes/index.js
+++ nodebird-api/routes/index.js
@@ -1,13 +1,11 @@
 const express = require('express');
-
+const {v4: uuidv4} = require('uuid');
 const {User, Domain} = require('../models');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  User.fineOne({
-    where: {
-      id: req.user && req.user.id,
-    },
+  User.findOne({
+    where: {id: (req.user && req.user.id) || null},
     include: {model: Domain},
   })
     .then((user) => {
@@ -22,4 +20,20 @@ router.get('/', (req, res, next) => {
       next(e);
     });
 });
+
+router.post('/domain', (req, res, next) => {
+  Domain.create({
+    userId: req.user.id,
+    host: req.body.host,
+    type: req.body.type,
+    clientSecret: uuidv4(),
+  })
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
 module.exports = router;
