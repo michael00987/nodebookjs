58881ce3745a57eb1147ddbc5ad824a3420cf676
michael00987 <hyeongkwangseok@gmail.com>
2020-10-03 14:27:05 +0900

    Socket.io

--- gif-chat/socket.js
+++ gif-chat/socket.js
@@ -1,26 +1,29 @@
-const WebSocket = require('ws');
+const SocketIo = require('socket.io');
 
 module.exports = (server) => {
-  const wss = new WebSocket.Server({server});
+  const io = SocketIo(server, {path: '/socket.io'}); // 클라이언트의 path 와 일치해야 한다.
 
-  wss.on('connection', (ws, req) => {
+  io.on('connection', (socket) => {
+    const req = socket.request; // 클라이언트에서 넘어오는 데이터는 socket.request 에 담긴다.
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    console.log('새로운 클라이언트 접속', ip);
-    ws.on('message', (message) => {
-      console.log(message);
+    console.log('새로운 클라이언트 접속!!', ip, socket.id, req.ip);
+    // disconnect 와 error 는 정해져 있는 이벤트 이름이다.
+    socket.on('disconnect', () => {
+      console.log('클라이언트 접속 해제', ip, socket.id);
+      clearInterval(socket.interval);
     });
-    ws.on('error', (error) => {
+    socket.on('error', (error) => {
       console.error(error);
     });
-    ws.on('close', () => {
-      console.log('클라이언트 접속 해제', ip);
-      clearInterval(ws.interval);
+    // message, relpay 는 다른 이름으로 해도 된다. 그리고 여러개를 만들수 있다.
+    socket.on('message', (data) => {
+      console.log(data);
+    });
+    socket.on('reply', (data) => {
+      console.log(data);
     });
     const interval = setInterval(() => {
-      if (ws.readyState === ws.OPEN) {
-        ws.send('서버에서 클라이언트로 메시지를 보냅니다.');
-      }
+      socket.emit('news', 'Hello Socket.io'); // 프론트에서 on('news')로 받을수 있다.
     }, 3000);
-    ws.interval = interval;
   });
 };
